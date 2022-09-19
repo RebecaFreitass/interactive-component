220a1f4a014bc510236f12eff7edf25ad5bb317b
rebecasguerri <rebecasguerri@gmail.com>
2022-09-19 19:41:13 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const btns = document.querySelectorAll('.btn');
+const textValue =document.querySelector("#value-here");
 
+
+btns.forEach( (btn) => {
+     btn.addEventListener("click",() =>{
+       btn.style.backgroundColor= "hsl(25, 97%, 53%)";
+
+       textValue.innerHTML = `You selected ${btn.value} out of 5`;
+     });
+   
+});
