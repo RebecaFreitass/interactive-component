4eb1c417e7662359968fb78c6bc4021bfe0ea382
rebecafreitass <rebecasguerri@gmail.com>
2023-03-28 17:14:43 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const main = document.querySelector(".card1");
 const thanksContent = document.querySelector(".card2");
+const submitButton = document.querySelector(".submit");
+const startAgainButton = document.querySelector(".start-again");
+const rating = document.querySelector("#rating")
+const rates = document.querySelectorAll(".numbers-content")
 
+submitButton.addEventListener("click", ()=>{
+    thanksContent.classList.remove("hidden")
+    main.style.display="none"
+});
+
+startAgainButton.addEventListener("click", ()=>{
+    thanksContent.classList.add("hidden")
+    main.style.display="block"
+   
+})
+
+rates.forEach((rate)=>{
+    rate.addEventListener("click", () =>{
+        console.log(rate.innerHTML)
+        rating.innerHTML = rate.innerHTML
+    })
+})
